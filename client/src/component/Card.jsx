6bfea117169952b9639ebe0da6263cd158612342
Rijal666 <rizkirizalmualim2@gmail.com>
2023-03-20 00:42:04 +0700

    deploy

--- client/src/component/Card.jsx
+++ client/src/component/Card.jsx
@@ -25,8 +25,8 @@ const Home = () => {
       <Container className="d-flex my-3 gap-4 row-cols-4" style={{ marginLeft: 170 }}>
       {asceding?.map((item) => {
         return (
-          <div key={item.id} className="my-3 mb-5" style={{ backgroundColor: "#F7E6DA", width: 241, height: 410 }}>
-            <Link className="text-decoration-none" to={`/detail-product/${item.id}`} style={{marginLeft:"10px"}}>
+          <div key={item.id} className="my-3 mb-5" style={{ backgroundColor: "#F7E6DA", width: 241, height: 410, marginLeft:"10px" }}>
+            <Link className="text-decoration-none" to={`/detail-product/${item.id}`}>
               <Card.Img variant="top" src={`${item.photo}`} to="/detail-product" />
               <Card.Body style={{ color: "#613D2B", fontSize: 14 }}>
                 <Card.Title className="fw-bold" style={{marginTop: 14, marginLeft: 16, color: "#613D2B"}} >{item.name}</Card.Title>
